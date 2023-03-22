7c1d0668a1fbceca6b1a67905199d2eb56ee647e
Grigory Smolkin <gsmolkin@ozon.ru>
2023-03-22 13:41:55 +0300

    sanitize env

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -17,6 +17,8 @@ fi
 
 # sanitize environment
 apt-get purge -y $(dpkg -l | awk '{print$2}' | grep postgres)
+which pg_config
+exit 5
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
@@ -69,5 +71,6 @@ pip3 install testgres
 # Build and install pg_probackup (using PG_CPPFLAGS and SHLIB_LINK for gcov)
 echo "############### Compiling and installing pg_probackup:"
 export PG_SRC=$PWD
+export PATH=$PGHOME/bin:$PATH
 # make USE_PGXS=1 PG_CPPFLAGS="-coverage" SHLIB_LINK="-coverage" top_srcdir=$CUSTOM_PG_SRC install
 make USE_PGXS=1 top_srcdir=$PG_SRC install
