89880f44a794f4d0316113b4e670d1b5544db130
Mikhail A. Kulagin <kulaginm@gmail.com>
2023-03-25 21:42:36 +0300

    Revert "Add --start-time option for backup"
    This reverts commit 3c74ebf2f9a4eb22b9a9dfb955d3379e5c217f48.

--- src/pg_probackup.c
+++ src/pg_probackup.c
@@ -200,7 +200,6 @@ static ConfigOption cmd_options[] =
 	{ 's', 'i', "backup-id",		&backup_id_string,	SOURCE_CMD_STRICT },
 	{ 'b', 133, "no-sync",			&no_sync,			SOURCE_CMD_STRICT },
 	{ 'b', 134, "no-color",			&no_color,			SOURCE_CMD_STRICT },
-	{ 'U', 241, "start-time",		&start_time,		SOURCE_CMD_STRICT },
 	/* backup options */
 	{ 'b', 180, "backup-pg-log",	&backup_logs,		SOURCE_CMD_STRICT },
 	{ 'f', 'b', "backup-mode",		opt_backup_mode,	SOURCE_CMD_STRICT },
@@ -966,9 +965,10 @@ main(int argc, char *argv[])
 			return do_init(catalogState);
 		case BACKUP_CMD:
 			{
+				time_t start_time;
+				time(&start_time);
+
 				current.stream = stream_wal;
-				if (start_time == 0)
-					start_time = current_time;
 
 				/* sanity */
 				if (current.backup_mode == BACKUP_MODE_INVALID)
